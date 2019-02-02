f47e7c80c79abd4ccd1f5ec03d440fb202f3bcd4
Jeff Palmer <palmer1026@gmail.com>
2019-02-02 10:32:04 -0600

    jsx basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Welcome to the Road to learn React';
+    const user = {
+      firstName: 'Jeff',
+      lastName: 'Palmer'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>{helloWorld}</h2>
+        <h3>{user.firstName} {user.lastName}</h3>
       </div>
     );
   }
