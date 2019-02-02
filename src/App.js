ab3f2696ad889e6e73000a688211fc88bdccf59a
Jeff Palmer <palmer1026@gmail.com>
2019-02-02 10:52:54 -0600

    Modify App component to render a list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome to the Road to learn React';
-    const user = {
-      firstName: 'Jeff',
-      lastName: 'Palmer'
-    };
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
-        <h3>{user.firstName} {user.lastName}</h3>
+          {list.map(function(item) {
+              return <div>{item.title}</div>;
+          })}
       </div>
     );
   }
