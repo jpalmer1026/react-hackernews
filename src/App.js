342d86c04b5878eec74f78543ac6af8cbe9369b6
Jeff Palmer <palmer1026@gmail.com>
2019-02-06 15:49:56 -0600

    Add functionality to search items in list

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ const list = [
   },
 ];
 
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -27,10 +33,17 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
 
   onDismiss(id) {
     const updatedList = this.state.list.filter(item => item.objectID !== id);
@@ -40,7 +53,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input type="text"
+                 onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
           <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
